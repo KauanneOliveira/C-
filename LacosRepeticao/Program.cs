5e1a166473b45091fff7c89ff038851a4fda4ef3
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-03-07 14:52:05 -0300

    Add files via upload

---
+++ LacosRepeticao/Program.cs
@@ -0,0 +1,11 @@
+﻿var contador = 15;
+do{
+    Console.WriteLine("O contador vale: " +contador);
+    contador++;
+}while(contador <=10); 
+
+Console.WriteLine();
+
+for(int i=1; i<=10; i++){
+    Console.WriteLine("A variável i agora vale " +i);
+}
