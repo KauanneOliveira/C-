9875e62d384070542b4474156d8b4a3be419bac3
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-03-07 15:24:23 -0300

    Add files via upload

---
+++ UsandoArray2/Program.cs
@@ -0,0 +1,24 @@
+﻿string aulaIntro = "Introdução às Coleções";
+
+string aulaModelando = "Modelando a Classe Aula";
+
+string aulaSets = "Trabalhando com Conjuntos";
+
+string[] aulas = new string[3];
+
+aulas[0] = aulaIntro;
+
+aulas[1] = aulaModelando;
+
+aulas[2] = aulaSets;
+
+for (int i=0; i<aulas.Length; i++){
+    Console.WriteLine(aulas[i]);
+}
+
+Console.WriteLine("Aula Modelando está no índice " +Array.IndexOf(aulas, aulaModelando));
+ 
+Console.WriteLine("Aula Modelando está no índice " +Array.LastIndexOf(aulas, aulaModelando)); 
+
+ 
+
