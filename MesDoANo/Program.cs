c5e21ea01a7bd459298421e432d329479685f38e
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-03-07 14:52:48 -0300

    Add files via upload

---
+++ MesDoANo/Program.cs
@@ -0,0 +1,16 @@
+﻿int mes = 2;
+
+switch(mes){
+    case 1:
+        Console.WriteLine("O mês é Janeiro!");
+        break;
+    case 2:
+        Console.WriteLine("O mês é Fevereiro!");
+        break;
+    case 3:
+        Console.WriteLine("O mês é Março!");
+        break;
+    default:
+        Console.WriteLine("Mês Inválida!");
+        break;
+} 
