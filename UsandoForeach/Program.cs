4c246ad378792cb26a9b0974784f43261e8e593f
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-03-07 15:15:23 -0300

    Add files via upload

---
+++ UsandoForeach/Program.cs
@@ -0,0 +1,5 @@
+﻿String[] alunos = new string[5] {"Kauanne", "Jujuba", "Marceline", "Finn", "Hazel"};
+
+foreach(var aluno in alunos) {  //é melhor pq se alterar o array referene ao tamanho e tal, eu não preciso mexer no foreach
+    Console.WriteLine(aluno);  // aluno é uma variavel contadora
+} 
