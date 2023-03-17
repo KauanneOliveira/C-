d63d75e381d14ec2ee53c1d34b6cc3dcace44a1e
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-03-17 00:45:34 -0300

    Update Program.cs

--- Calculadora2/Program.cs
+++ Calculadora2/Program.cs
@@ -1,7 +1,8 @@
 ﻿class Calculadora {
     public static double Operacao( double num1, double num2, string opcao){
 
-        double resultado= double.NaN;   //not-a-number. NaN é uma variável associada ao TryParse, se o TryParse reconhecer a informação q está entrando como um número ela desabilita o true do NaN e coloca esse dado na variável, caso contrário ela continue como true, ou seja, sem número.
+        double resultado= double.NaN;          // Um valor Not-a-Number (NaN) pode ser interpretado como um valor indefinido ou irrepresentável, especialmente em aritmética de ponto flutuante. Em C#  pode comparar NaN com double usando o ==.
+                                               
         switch(opcao){
             case "a":
                resultado= num1 + num2;
@@ -44,14 +45,14 @@ class Programa {
              n1 = Console.ReadLine();
 
              while( ! double.TryParse(n1, out num1) ){                               // caso colocar uma letra ou algo q não seja número, vai ficar nesse laço até colocar um número
-                 Console.WriteLine("Número Inválido. Digite um número válido:");     // TryParse resulta em um booleano informando se a conversão foi possível e caso verdade, o valor da conversão.
-                 n1= Console.ReadLine();                                             // no parâmetro do TryParse o primeiro valor é o dado entrado pelo usuário e o segundo parâmetro é a resposta, então se dado digitado for um número o TryParse vai reconhecer e vai mandar o valor para a variável de saída 
+                 Console.WriteLine("Número Inválido. Digite um número válido:");     // TryParse() são usados para converter uma representação de string de um número em um inteiro. Ele resulta em um booleano informando se a conversão foi possível e caso verdade, o valor da conversão.
+                 n1= Console.ReadLine();                                             // no parâmetro do TryParse o primeiro valor é o dado entrado pelo usuário e o segundo parâmetro é a resposta, então se dado digitado for um número o TryParse vai reconhecer, converter e vai mandar o valor para a variável de saída 
              }
 
              Console.WriteLine("Digite outro número válido:");                       // depois de ser colocado um número, vai pedir para colocar o segundo numero
              n2= Console.ReadLine();
 
-             while( ! double.TryParse(n2, out num2) ){                               //se não for número, fica nesse laço até ser um 
+             while( ! double.TryParse(n2, out num2) ){                               //se não for um número, fica nesse laço até ser um 
                  Console.WriteLine("Número Inválido. Digite outro número válido:");
                  n2= Console.ReadLine();
              }
@@ -68,8 +69,8 @@ class Programa {
              try {
                  resultado= Calculadora.Operacao( num1, num2, opcao);
 
-                 if( double.IsNaN(resultado) ){  
-                     Console.WriteLine("Esta operação resultará em um erro matemático.\n");
+                 if( double.IsNaN(resultado) ){                                                   // Usa o  método IsNaN para determinar se um valor não é um número.
+                     Console.WriteLine("Esta operação resultará em um erro matemático.\n");       //As operações de ponto flutuante NaN (double.IsNaN) retornam true (ou false se não for) para sinalizar que o resultado da operação é indefinido (NaN). Por exemplo, dividir 0,0 por 0,0 resulta em NaN .
                  } 
                  else
                      Console.WriteLine("Seu resultado é : {0:0.####}\n", resultado);     // 0:0.#### = vai mostrar o resultado com 4 números depois da vírgula.
