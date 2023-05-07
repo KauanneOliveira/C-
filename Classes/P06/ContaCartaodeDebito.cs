f206019eaaa9b4d30fe134c19779d05b78203cf0
Kauanne Oliveira <paulakauanne2411@gmail.com>
2023-05-07 16:29:17 -0300

    Update ContaCartaodeDebito.cs

--- Classes/P06/ContaCartaodeDebito.cs
+++ Classes/P06/ContaCartaodeDebito.cs
@@ -1,8 +1,10 @@
 namespace Classes;
-public class ContaCartaodeDebito : ContaBancaria
+public class ContaCartaodeDebito : ContaBancaria  //essa classe está herdando métodos e comportamentos da classe ContaBancaria
 {
     private readonly decimal _depositosMensais = 0m;
 
+    //Os parâmetros para esse novo construtor correspondem ao tipo de parâmetro e aos nomes do construtor de classe base
+    // ":base()" serve para indicar uma chamada para um construtor de classe base(ContaBancaria)
     public ContaCartaodeDebito(string nome, decimal saldoInicial, decimal depositosMensais = 0) : base(nome, saldoInicial)
     => _depositosMensais = depositosMensais;
     public override void ExecutarTransacoesdeFimdeMes()
